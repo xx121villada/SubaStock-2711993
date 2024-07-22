4aa93e0099f0ab566ea365d054241a3edda9e5cb
Kex717 <sebasramirez20060224@gmail.com>
2024-07-21 21:08:09 -0500

    SUbiendo componente detalle subasta

--- SubaStock/src/AppRouter.jsx
+++ SubaStock/src/AppRouter.jsx
@@ -1,12 +1,15 @@
 import {Routes, Route, HashRouter} from 'react-router-dom'
 import React from 'react'
 import Inicio from './App'
+import {DetalleSubasta} from"./components/detalleSubasta/DetalleSubasta";
 
 export default function AppRouter() {
   return (
       <HashRouter>
         <Routes>
           <Route exact path="/" element={<Inicio />} />
+          <Route exact path="/detalle-subasta" element={<DetalleSubasta />} />
+
         </Routes>
       </HashRouter>
   )
