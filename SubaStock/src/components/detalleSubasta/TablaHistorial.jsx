27b20217b47be04fb75ff7f6d4dce015752c9a9f
Kex717 <sebasramirez20060224@gmail.com>
2024-08-07 20:24:31 -0500

    agregando detalle subasta y subastar

---
+++ SubaStock/src/components/detalleSubasta/TablaHistorial.jsx
@@ -0,0 +1,52 @@
+import "./detalleSubasta.css";
+
+export function TablaHistorial() {
+  return (
+    <div
+      className= "container-md my-2 mx-3 p-2 d-md-flex flex-column align-items-center historial"
+    >
+      <div
+        className="d-flex flex-column flex-md-row align-items-center m-1 p-1 justify-content-center"
+        id="titulo"
+      >
+        <h3 className="mb-2 mb-md-0 me-md-2">Historial De Pujas</h3>
+      </div>
+      <div className="table-responsive">
+        <table className="table table-striped text-center">
+          <thead>
+            <tr>
+              <th scope="col">Nombre</th>
+              <th scope="col">Valor</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">J***n</th>
+              <td>2.200.000</td>
+            </tr>
+            <tr>
+              <th scope="row">K***a</th>
+              <td>2.100.000</td>
+            </tr>
+            <tr>
+              <th scope="row">L*****s</th>
+              <td>1.900.000</td>
+            </tr>
+            <tr>
+              <th scope="row">J***n</th>
+              <td>2.200.000</td>
+            </tr>
+            <tr>
+              <th scope="row">K***a</th>
+              <td>2.100.000</td>
+            </tr>
+            <tr>
+              <th scope="row">L*****s</th>
+              <td>1.900.000</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
---
+++ SubaStock/src/components/subastar/Subastar.jsx
@@ -0,0 +1,49 @@
+import "./subastar.css"
+
+export function Subastar() {
+  
+  return (
+    <div className="body container-md p-2 d-flex flex-column align-items-center">
+      <div className="w-100 d-flex justify-content-start align-items-center mb-3">
+        <button className="back-button ms-2">
+          <i className="bi bi-arrow-bar-left"></i> Regresar
+        </button>
+      </div>
+
+      <div className="d-flex flex-column flex-md-row align-items-center justify-content-between">
+        
+        <div className="content-agregar-imagen">
+          <h6>Agregar Imagenes</h6>
+          <img src="https://cdn-icons-png.flaticon.com/512/4211/4211763.png" alt="agregar imagen" className="agregar-img"/>
+        </div>
+
+        <div className="d-flex flex-column d-md-flex-row info">
+          <div className="d-flex flex-row flex-md-row align-items-center justify-content-center mb-3 content-titulo">
+            <input type="text" className="titulo mt-4" placeholder="Ingrese El Titulo"></input>
+          </div>
+          <h6 className="tiempoRestante mb-2 fs-3">Fechas Limites</h6>
+          <div className="fechas mb-3 text-center d-flex flex-column">
+            <h6 className="my-3 ">Fecha Inicio:</h6>
+            <input className="d-block mx-1 my-1" type="date" ></input>
+            <h6 className="my-3">Fecha Cierre:</h6>
+            <input className="d-block mx-1 my-1" type="date"></input>
+          </div>
+          <div className="d-flex flex-column my-2 text-center">
+          <h6 className="mb-3">Descripcion:</h6>
+          <input  className="descripcion mb-3" placeholder="Ingrese Descripcion"></input>
+          </div>
+          
+          <div className="valor-inicial w-100 d-flex flex-column justify-content-center text-align-center align-items-center mb-3">
+            <h6>Valor Inicial De La Puja:</h6>
+            <input
+              type="number"
+              className="mx-2 my-2 w-50 input-valor"
+              placeholder="ingrese el valor inicial"
+            />
+            <button className="subastar">Subastar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
