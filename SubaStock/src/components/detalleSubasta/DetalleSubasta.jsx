28631aac20ac156b3b0429a725d3a7e9de6b0cf7
Kex717 <sebasramirez20060224@gmail.com>
2024-08-27 16:17:33 -0500

    Volviendo componentes css

--- SubaStock/src/components/detalleSubasta/DetalleSubasta.jsx
+++ SubaStock/src/components/detalleSubasta/DetalleSubasta.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import "./detalleSubasta.css";
+import styles from "./detalleSubasta.module.css";
 import { TablaHistorial } from "./TablaHistorial";
-import { Carrusel } from "./Carrusel";
+import LazyCarousel from "../Subastas/LazyCarousel";
 
 export function DetalleSubasta() {
   const [verTabla, setVerTabla] = useState(false);
@@ -11,42 +11,45 @@ export function DetalleSubasta() {
   };
 
   return (
-    <div className="body container-md p-2 d-flex flex-column align-items-center">
+    <div className={`container-md p-2 d-flex flex-column align-items-center ${styles.body}`}>
       <div className="w-100 d-flex justify-content-start align-items-center mb-3">
-        <button className="back-button ms-2">
+        <button className={styles.backButton}>
           <i className="bi bi-arrow-bar-left"></i> Regresar
         </button>
       </div>
 
       <div className="d-flex flex-column flex-md-row align-items-center justify-content-between">
-        
-        <div className="content-carrusel flex-grow-1">
-          <Carrusel />
+        <div className={styles.contentCarrusel}>
+          <LazyCarousel imgs={
+          [
+            "https://wintechnology.co/wp-content/uploads/2021/11/imagen-no-disponible.jpg",
+          ]
+        }/>
         </div>
 
-        <div className="d-flex flex-column d-md-flex-row info">
-          <div className="d-flex flex-row flex-md-row align-items-center justify-content-center mb-3 titulo-favoritos">
-            <h2 className="titulo mb-2 mb-md-0 me-md-2">Toro Cebú</h2>
-            <button className="favoritos">☆</button>
+        <div className={`d-flex flex-column d-md-flex-row ${styles.info}`}>
+          <div className={`d-flex flex-row flex-md-row align-items-center justify-content-center mb-3 ${styles.tituloFavoritos}`}>
+            <h2 className={`${styles.titulo} mb-2 mb-md-0 me-md-2`}>Toro Cebú</h2>
+            <button className={styles.favoritos}>☆</button>
           </div>
-          <p className="tiempoRestante mb-3">Cierra en 2 días</p>
-          <div className="fechas mb-3 text-center">
+          <p className={`mb-3 ${styles.tiempoRestante}`}>Cierra en 2 días</p>
+          <div className={`mb-3 text-center ${styles.fechas}`}>
             <span className="d-block mx-1">Fecha Cierre: 23/06/2024</span>
             <span className="d-block mx-1">Fecha Apertura: 01/06/2024</span>
           </div>
-          <p className="ofertaActual mb-3">Oferta Actual: 6.560.000 COP</p>
-          <p className="descripcion mb-3">
+          <p className={`mb-3 ${styles.ofertaActual}`}>Oferta Actual: 6.560.000 COP</p>
+          <p className={`mb-3 ${styles.descripcion}`}>
             Toro cebú de 570 kilos, vacunas al día.
           </p>
-          <div className="puja w-100 d-flex flex-column justify-content-center flex-md-row align-items-center mb-3">
+          <div className={`w-100 d-flex flex-column justify-content-center flex-md-row align-items-center mb-3 ${styles.puja}`}>
             <input
               type="number"
-              className="mx-2 my-2 w-50 input-puja"
+              className={`mx-2 my-2 w-50 ${styles.inputPuja}`}
               placeholder="Realice su puja"
             />
-            <button className="pujar">Pujar</button>
+            <button className={styles.pujar}>Pujar</button>
           </div>
-          <button className="historialPujas w-50" onClick={toggleTabla}>
+          <button className={`w-50 ${styles.historialPujas}`} onClick={toggleTabla}>
             HISTORIAL DE PUJAS
           </button>
         </div>
--- SubaStock/src/components/subastar/Subastar.jsx
+++ SubaStock/src/components/subastar/Subastar.jsx
@@ -1,46 +1,48 @@
-import "./subastar.css"
+import styles from "./subastar.module.css";
 
 export function Subastar() {
-  
   return (
-    <div className="body container-md p-2 d-flex flex-column align-items-center bg-white">
+    <div className={`container-md p-2 d-flex flex-column align-items-center bg-white ${styles.body}`}>
       <div className="w-100 d-flex justify-content-start align-items-center mb-3">
-        <button className="back-button ms-2">
+        <button className={`ms-2 ${styles.backButton}`}>
           <i className="bi bi-arrow-bar-left"></i> Regresar
         </button>
       </div>
 
       <div className="d-flex flex-column flex-md-row align-items-center justify-content-between">
-        
-        <div className="content-agregar-imagen">
-          <h6>Agregar Imagenes</h6>
-          <img src="https://cdn-icons-png.flaticon.com/512/4211/4211763.png" alt="agregar imagen" className="agregar-img"/>
+        <div className={styles.contentAgregarImagen}>
+          <h6 className={`${styles.tituloAgregar}`}>Agregar Imagenes</h6>
+          <img
+            src="https://cdn-icons-png.flaticon.com/512/4211/4211763.png"
+            alt="agregar imagen"
+            className={styles.agregarImg}
+          />
         </div>
 
-        <div className="d-flex flex-column d-md-flex-row info">
-          <div className="d-flex flex-row flex-md-row align-items-center justify-content-center mb-3 content-titulo">
-            <input type="text" className="titulo mt-4" placeholder="Ingrese El Titulo"></input>
+        <div className={`d-flex flex-column d-md-flex-row ${styles.info}`}>
+          <div className={`d-flex flex-row flex-md-row align-items-center justify-content-center mb-3 ${styles.contentTitulo}`}>
+            <input type="text" className={`mt-4 ${styles.titulo}`} placeholder="Ingrese El Titulo" />
           </div>
-          <h6 className="tiempoRestante mb-2 fs-3">Fechas Limites</h6>
-          <div className="fechas mb-3 text-center d-flex flex-column">
-            <h6 className="my-3 ">Fecha Inicio:</h6>
-            <input className="d-block mx-1 my-1" type="date" ></input>
+          <h6 className={`mb-2 fs-3 ${styles.tiempoRestante}`}>Fechas Limites</h6>
+          <div className={`mb-3 text-center d-flex flex-column ${styles.fechas}`}>
+            <h6 className="my-3">Fecha Inicio:</h6>
+            <input className="d-block mx-1 my-1" type="date" />
             <h6 className="my-3">Fecha Cierre:</h6>
-            <input className="d-block mx-1 my-1" type="date"></input>
+            <input className="d-block mx-1 my-1" type="date" />
           </div>
           <div className="d-flex flex-column my-2 text-center">
-          <h6 className="mb-3">Descripcion:</h6>
-          <input  className="descripcion mb-3" placeholder="Ingrese Descripcion"></input>
+            <h6 className="mb-3">Descripcion:</h6>
+            <input className={`mb-3 ${styles.descripcion}`} placeholder="Ingrese Descripcion" />
           </div>
-          
-          <div className="valor-inicial w-100 d-flex flex-column justify-content-center text-align-center align-items-center mb-3">
+
+          <div className={`w-100 d-flex flex-column justify-content-center text-align-center align-items-center mb-3 ${styles.valorInicial}`}>
             <h6>Valor Inicial De La Puja:</h6>
             <input
               type="number"
-              className="mx-2 my-2 w-50 input-valor"
-              placeholder="ingrese el valor inicial"
+              className="mx-2 my-2 w-50"
+              placeholder="Ingrese el valor inicial"
             />
-            <button className="subastar">Subastar</button>
+            <button className={styles.subastar}>Subastar</button>
           </div>
         </div>
       </div>
