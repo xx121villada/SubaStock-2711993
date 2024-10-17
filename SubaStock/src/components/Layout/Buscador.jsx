17c855500239ec6e91d38399df230b95777570ab
JuanjoG11 <estevanho01@gmail.com>
2024-10-17 16:58:17 -0500

    css en modulos

--- SubaStock/src/components/Layout/Buscador.jsx
+++ SubaStock/src/components/Layout/Buscador.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import './Buscador.css'; 
+import styles from './Buscador.module.css'; 
 
 const Buscador = () => {
   const [textoBusqueda, setTextoBusqueda] = useState("");
@@ -92,16 +92,16 @@ const Buscador = () => {
       </div>
 
       {animales.length > 0 && (
-        <div className="resultados-busqueda">
+        <div className={styles.resultadosBusqueda}>
           {animales.map((animal) => (
             <Link
               key={animal.idAnimal}
               to={`/crud-animal/${animal.idAnimal}`}
               
-              className="resultado-item-link"
+              className={styles.resultadoItemLink}
               onClick={() => handleAnimalClick(animal.idAnimal, animal.marca, animal.raza)}
               >
-              <div className="resultado-item">
+              <div className={styles.resultadoItem}>
                 <h5>{animal.raza} - {animal.marca}</h5>
               </div>
             </Link>
