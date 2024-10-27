c53e7092853f5dff079a6e2792309a6574feb9d5
Camilo <juancamilog9911@gmail.com>
2024-10-27 10:46:30 -0500

    cambios en gama de colores de componentes

--- SubaStock/src/pages/Animales/CardTiposEspecie.jsx
+++ SubaStock/src/pages/Animales/CardTiposEspecie.jsx
@@ -9,13 +9,17 @@ export default function CardTiposEspecie({ tipoEspecie, imagen }) {
 
     return (
         <div className={styles.cardContainer}>
-            <img
-                src={imagen}
-                alt={tipoEspecie.especie}
-                className={styles.cardImage}
-            />
-            <h5 className={styles.cardTitle}>{tipoEspecie.especie}</h5>
-            <Link to={`/visualizar/${tipoEspecie.especie.toLowerCase()}`} onClick={handleClick}>
+            <Link 
+                to={`/visualizar/${tipoEspecie.especie.toLowerCase()}`} 
+                onClick={handleClick} 
+                className={styles.linkCard}
+            >
+                <img 
+                    src={imagen} 
+                    alt={tipoEspecie.especie} 
+                    className={styles.cardImage} 
+                />
+                <h5 className={styles.cardTitle}>{tipoEspecie.especie}</h5>
                 <button className={styles.cardButton}>Visualizar</button>
             </Link>
         </div>
